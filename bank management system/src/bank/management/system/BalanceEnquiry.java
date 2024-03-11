7a06bf3f8dd9d9420f14b2b30027b09a64c34a3d
Faizan12456 <143254929+Faizan12456@users.noreply.github.com>
2024-03-12 01:19:47 +0530

    Add files via upload

---
+++ bank management system/src/bank/management/system/BalanceEnquiry.java
@@ -0,0 +1,62 @@
+package bank.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+
+public class BalanceEnquiry extends JFrame implements ActionListener{
+    
+    JButton back;
+    String pinnumber;
+    
+    BalanceEnquiry(String pinnumber) {
+        this.pinnumber = pinnumber;
+        setLayout(null);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(800, 750, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0, 0, 800, 750);
+        add(image);
+        
+        back = new JButton("Back");
+        back.setBounds(300, 430, 150, 25);
+        back.addActionListener(this);
+        image.add(back);
+        
+        Conn c = new Conn();
+        int balance = 0;
+        try {
+            ResultSet rs = c.s.executeQuery("select * from bank where pin = '"+pinnumber+"'");
+            while(rs.next()) {
+                if(rs.getString("type").equals("Deposit")) {
+                    balance += Integer.parseInt(rs.getString("amount"));
+                }else {
+                    balance -= Integer.parseInt(rs.getString("amount"));
+                }
+            }
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+        
+        JLabel text = new JLabel("Your Current Account balance is Rs " + balance);
+        text.setForeground(Color.WHITE);
+        text.setBounds(150, 250, 400, 30);
+        image.add(text);
+        
+        setSize(800, 750);
+        setLocation(300, 0);
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        setVisible(false);
+        new Transaction(pinnumber).setVisible(true);
+    }
+    
+    public static void main(String args[]) {
+        new BalanceEnquiry("");
+    }
+}
